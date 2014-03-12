99fe1fddcf220e4eccef7359b7978c2a6300a276
Vincent AVEZ <vertignasse@free.fr>
2014-03-12 20:31:37 +0100

    modified main.c, fonctional

--- src/main.c
+++ src/main.c
@@ -1,6 +1,16 @@
-#include <stdio.h>
 #include <stdint.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/ioctl.h>
+#include <scsi/sg.h>
+#include <inttypes.h>
 
+#define INQ_REPLY_LEN 96
+#define INQ_CMD_CODE 0x12
+#define INQ_CMD_LEN 6
 
 int main(int argc, char **argv)
  {
@@ -9,7 +19,7 @@ int main(int argc, char **argv)
 
   uint8_t sense[32];
   uint8_t data[2];
-  uint8_t cdb[16] = { 0xdf, 0x10, 0x00, 0x00, 0x02, 0x00,0x00, 0x00, 0x00, 0x00, 0x00, 0xff,0x00, 0x00, 0x00, 0x00};
+  uint8_t cdb[16] = { 0xdf, 0x10, 0x00, 0x00, 0x02, 0x00,0x00, 0x00, 0x00, 0x00, 0x00, 0xff}; //,0x00, 0x00, 0x00, 0x00};//
 
   fd = open (argv[1], O_RDWR);
   
@@ -25,12 +35,14 @@ int main(int argc, char **argv)
   sg_io.sbp = sense;
   sg_io.mx_sb_len = sizeof(sense);
   sg_io.timeout = 5000; // 5 seconds
+  
+  
   rc = ioctl (fd, SG_IO, &sg_io);
 
   if ((data[1] & 0x01) == 0)
-    printf("button is pressed");
+    printf("button is pressed\n");
   else
-    printf("button is NOT pressed");
+    printf("button is NOT pressed\n");
 
   return rc;
  }
